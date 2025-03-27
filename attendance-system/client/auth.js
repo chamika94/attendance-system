58f33dd9168e77f69c8f3f78b3a66028b850235a
chamika94 <chamikasandaruwan1994@gmail.com>
2025-03-27 13:29:45 +0530

    #14, prevented logged-in users from accessing login page again

--- attendance-system/client/auth.js
+++ attendance-system/client/auth.js
@@ -5,9 +5,19 @@ function logout() {
 
 document.addEventListener("DOMContentLoaded", () => {
     const token = localStorage.getItem("token");
-    if (!token) {
-        window.location.href = "index.html"; 
-    } else {
-        document.getElementById("welcome-msg").textContent = "You are logged in!";
+    if (window.location.pathname.includes("index.html")) {
+        if (token) {
+            window.location.href = "home.html";
+        }
+    } 
+    else if (window.location.pathname.includes("home.html")) {
+        if (!token) {
+            window.location.href = "index.html";
+        } else {
+            const welcomeMsg = document.getElementById("welcome-msg");
+            if (welcomeMsg) {
+                welcomeMsg.textContent = "You are logged in!";
+            }
+        }
     }
 });
